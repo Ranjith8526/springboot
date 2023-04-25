b936e5ecb8eab116aa81f9373df8214964100a41
Ranjith8526 <113403721+Ranjith8526@users.noreply.github.com>
2023-04-24 22:45:44 -0700

    commit 2

---
+++ demoPgm1/src/main/java/com/example/demo/DemoPgm1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPgm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPgm1Application.class, args);
+	}
+
+}
